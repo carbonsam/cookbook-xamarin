e9aad360da25828745c499ed477b6a6786905eff
Samuel Schumacher <samuelschu@pm.me>
2020-06-23 10:24:14 -0500

    Add iOS/Android custom renderer projects (#2)

---
+++ CookbookXamarin/FormsCustomRendererImplementationsAndroid/ViewPositionLocator.cs
@@ -0,0 +1,24 @@
+﻿using CustomRendererContracts;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace FormsCustomRendererImplementationsAndroid
+{
+    public class ViewPositionLocator : IViewPositionLocator
+    {
+        public Rectangle GetCoordinates(VisualElement view)
+        {
+            var nativeView = Platform.GetRenderer(view).View;
+            var screenDensity = nativeView.Context.Resources.DisplayMetrics.Density;
+            var locationPoints = new int[2];
+
+            nativeView.GetLocationOnScreen(locationPoints);
+
+            int absX = (int)Math.Round(locationPoints[0] / screenDensity);
+            int absY = (int)Math.Round(locationPoints[1] / screenDensity);
+
+            return new Rectangle(absX, absY, view.Width, view.Height);
+        }
+    }
+}
