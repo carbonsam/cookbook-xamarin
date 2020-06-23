0992e03c1efeaf265b668bb143e4d35a11eb4481
Samuel Schumacher <samuel.schumacher@daveramsey.com>
2020-06-23 10:16:35 -0500

    add custom renderer projects

---
+++ CookbookXamarin/FormsCustomRendererImplementationsIOS/ViewPositionLocator.cs
@@ -0,0 +1,21 @@
+﻿using CustomRendererContracts;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace FormsCustomRendererImplementationsIOS
+{
+    public class ViewPositionLocator : IViewPositionLocator
+    {
+        public Rectangle GetCoordinates(VisualElement view)
+        {
+            var nativeView = Platform.GetRenderer(view).NativeView;
+            var absPoints = nativeView.Superview.ConvertPointToView(nativeView.Frame.Location, null);
+
+            int absX = (int)Math.Round(absPoints.X);
+            int absY = (int)Math.Round(absPoints.Y); // TODO: Account for iOS safe area
+
+            return new Rectangle(absX, absY, (int)view.Width, (int)view.Height);
+        }
+    }
+}
